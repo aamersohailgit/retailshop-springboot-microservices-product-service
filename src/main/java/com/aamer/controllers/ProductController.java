e8d4cfce6264400c063a66254afec4bbb6e87e29
aamer <aamer.developer@gmail.com>
2023-02-21 12:42:19 +1100

    handled empty @RequestBody values as No_Content error.

--- src/main/java/com/aamer/controllers/ProductController.java
+++ src/main/java/com/aamer/controllers/ProductController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.aamer.dto.Coupon;
 import com.aamer.model.Product;
@@ -19,31 +20,19 @@ import com.aamer.repos.ProductRepo;
 
 @RestController
 @RequestMapping(value = "/api/products")
-public class ProductController {
+public class ProductController extends ResponseEntityExceptionHandler {
 
 	@Autowired
 	ProductRepo productRepo;
-	
+
 	@Autowired
 	private RestTemplate restTemplate;
-	
+
 	@Value("${coupon-service-url}")
 	private String couponServiceUrl;
-	
-	
-	@PostMapping
-    public ResponseEntity<Product> createProduct(@RequestBody Product product) {
-		Coupon coupon = restTemplate.getForObject(couponServiceUrl+product.getCouponCode(), Coupon.class);
-		product.setPrice(product.getPrice().subtract(coupon.getDiscount()));
-        Product savedProduct = productRepo.save(product);
-        return new ResponseEntity<>(savedProduct, HttpStatus.CREATED);
-    }
 
-	
-	
-	
 	@GetMapping("/")
-	public List<Product> getProducts(){
+	public List<Product> getProducts() {
 		List<Product> products = null;
 		try {
 			products = productRepo.findAll();
@@ -51,6 +40,23 @@ public class ProductController {
 			System.out.println(e.getMessage());
 		}
 		return products;
-        
-    }
+	}
+
+	@PostMapping("/")
+	public ResponseEntity<Product> createProduct(@RequestBody Product product) {
+		Product savedProduct = null;
+		if (product.getPrice() == null || product.getName() == null) {
+			return new ResponseEntity<>(product, HttpStatus.NO_CONTENT);
+		}
+		try {
+			Coupon coupon = restTemplate.getForObject(couponServiceUrl + product.getCouponCode(), Coupon.class);
+			product.setPrice(product.getPrice().subtract(coupon.getDiscount()));
+			savedProduct = productRepo.save(product);
+
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+		}
+		return new ResponseEntity<>(savedProduct, HttpStatus.CREATED);
+	}
+
 }
